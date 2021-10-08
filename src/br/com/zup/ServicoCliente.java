70f84ec22e5a323fc581dc72b7804fe2a3276017
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-10-08 15:39:59 -0300

    Criar método cadastrarCliente na classe ServicoCliente

--- src/br/com/zup/ServicoCliente.java
+++ src/br/com/zup/ServicoCliente.java
@@ -1,5 +1,18 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoCliente {
+
+    public static List<Cliente>clientes = new ArrayList<>();
+
+    //Método cadastrar Cliente
+    public static Cliente cadastrarCliente(String nome, String cpf, String email){
+        Cliente cliente = new Cliente(nome, cpf, email);
+        clientes.add(cliente);
+        return cliente;
+    }
+
 
 }
