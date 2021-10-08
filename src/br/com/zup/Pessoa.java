ca6b1e417741ccd66a9ba6566ed5b7e722a46539
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-10-08 14:48:33 -0300

    Criar classe Pessoa

---
+++ src/br/com/zup/Pessoa.java
@@ -0,0 +1,46 @@
+package br.com.zup;
+
+public abstract class Pessoa {
+
+    //Atributos
+    private String nome;
+    private String cpf;
+    private String email;
+
+    //Métodos Construtores
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    //Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
