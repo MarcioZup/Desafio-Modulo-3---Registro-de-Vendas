42ab398f3353b0ecb1fa9a5c637b2507d5e55a96
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-10-08 14:54:07 -0300

    Modelar classe Cliente

--- src/br/com/zup/Cliente.java
+++ src/br/com/zup/Cliente.java
@@ -1,5 +1,10 @@
 package br.com.zup;
 
-public class Cliente {
+public class Cliente extends Pessoa{
+
+    //Atributos
+    public Cliente(String nome, String cpf, String email) {
+        super(nome, cpf, email);
+    }
 
 }
