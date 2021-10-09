29e023c3d0a7d3a633f42455a2b2df1d758da245
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-10-09 11:59:23 -0300

    Criar método para Menu na classe Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -10,4 +10,18 @@ public class Sistema {
         return new Scanner(System.in);
     }
 
+    private static void menu(){
+        System.out.println("-----------------------------");
+        System.out.println("Sistema de Registro de Vendas");
+        System.out.println("-----------------------------");
+        System.out.println("Selecione a opção desejada: ");
+        System.out.println("1 - Cadastro de Cliente");
+        System.out.println("2 - Consulta lista de Clientes ");
+        System.out.println("3 - Cadastra Vendedor Responsável pela Venda");
+        System.out.println("4 - Consulta lista de Vendedores");
+        System.out.println("5 - Cadastro de Vendas");
+        System.out.println("6 - Consulta Vendas");
+
+    }
+
 }
