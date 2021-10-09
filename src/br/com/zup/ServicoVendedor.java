7662e4827c1994f2598694d0107c124f6374ca5d
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-10-09 17:22:52 -0300

    Modelar classe serviço Vendedor com métodos adiciona/lista Vendedor

--- src/br/com/zup/ServicoVendedor.java
+++ src/br/com/zup/ServicoVendedor.java
@@ -1,4 +1,20 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoVendedor {
+    public static List<Vendedor> vendedores = new ArrayList<>();
+
+    //Método cadastrar Vendedor
+    public static Vendedor cadastrarVendedorResponsavel(String nome, String cpf, String email) throws Exception{
+        Vendedor vendedorResponsavel = new Vendedor(nome, cpf, email);
+        vendedores.add(vendedorResponsavel);
+        return vendedorResponsavel;
+    }
+
+    //Método mostrar lista de Vendedores cadastrados
+    public static void listarVendedores(){
+        System.out.println(vendedores.toString());
+    }
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -13,6 +13,7 @@ public class Sistema {
         return new Scanner(System.in);
     }
 
+    //Método Menu Principal
     private static void menu() {
         System.out.println("-----------------------------");
         System.out.println("Sistema de Registro de Vendas");
@@ -26,6 +27,7 @@ public class Sistema {
 
     }
 
+    //Método Cadastrar Cliente
     public static Cliente cadastrarCliente() throws Exception{
         String nome = entradaDeDados("Digite o nome do Cliente: ").nextLine();
         String cpf = entradaDeDados("Digite o CPF do Cliente: ").nextLine();
@@ -35,6 +37,17 @@ public class Sistema {
 
     }
 
+    //Método Cadastrar Vendedor que será responsável pelas Vendas
+    public static Vendedor cadastrarVendedorResponsavel() throws Exception{
+        String nome = entradaDeDados("Digite o nome do Vendedor: ").nextLine();
+        String cpf = entradaDeDados("Digite o CPF do Vendedor: ").nextLine();
+        String email = entradaDeDados("Digite o E-mail do Vendedor: ").nextLine();
+        System.out.println("Vendedor cadastrado com Sucesso!");
+        return ServicoVendedor.cadastrarVendedorResponsavel(nome, cpf, email);
+    }
+
+
+    //Método que executa o sistema
     public static boolean executar() throws Exception {
         menu();
         boolean loop = true;
@@ -42,8 +55,16 @@ public class Sistema {
             int opcao = entradaDeDados("\nDigite o número da opção desejada:").nextInt();
             if (opcao == 1) {
                 cadastrarCliente();
+                menu();
             } else if (opcao == 2) {
                 ServicoCliente.listarClientes();
+                menu();
+            }else if (opcao == 3){
+                cadastrarVendedorResponsavel();
+                menu();
+            }else if (opcao == 4){
+                ServicoVendedor.listarVendedores();
+                menu();
             }
         }
         return loop;
